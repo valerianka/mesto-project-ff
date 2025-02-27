377f2ddfe9cb45c9ad6cdf17093cf5c38a524d31
Valerie Gorbik <valerianka@me.com>
2025-02-26 19:48:23 -0800

    feature: creating and deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
 // @todo: Темплейт карточки
+const cardTemplateElement = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+let contentElement = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(card, removeCard) {
+  let newCardElement = cardTemplateElement
+    .querySelector(".card")
+    .cloneNode(true);
+
+  newCardElement.querySelector(".card__image").src = card.link;
+  newCardElement.querySelector(".card__title").textContent = card.name;
+  newCardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => removeCard(newCardElement));
+
+  return newCardElement;
+}
 
 // @todo: Функция удаления карточки
+let removeCard = function (cardElement) {
+  let cardsArray = contentElement.querySelectorAll(".card");
+
+  cardsArray.forEach((currentCardElement) => {
+    if (currentCardElement === cardElement) {
+      currentCardElement.remove();
+    }
+  });
+};
 
 // @todo: Вывести карточки на страницу
+function showCards(cards) {
+  cards.forEach((card) =>
+    contentElement.appendChild(createCard(card, removeCard)),
+  );
+}
+
+showCards(initialCards);
